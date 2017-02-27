5afaa9f1356cd1ab37e99c12e3379b5efe45f0cc
Craig Dowey <Craig Dowey>
2017-02-27 19:09:18 +0000

    users page working without photos

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -8,7 +8,7 @@
     <h1>Create Users</h1>
 
 
-    {!! Form::open(['method'=>'POST', 'action'=> 'AdminUsersController@store']) !!}
+    {!! Form::open(['method'=>'POST', 'action'=> 'AdminUsersController@store', 'files'=>true]) !!}
 
         <div class="form-group">
             {!! Form::label('name', 'Name:') !!}
@@ -16,10 +16,40 @@
         </div>
 
         <div class="form-group">
+            {!! Form::label('email', 'Email:') !!}
+            {!! Form::email('email', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('file', 'Photo:') !!}
+            {!! Form::file('file', null, ['class'=>'form-control']) !!}
+        </div>
+
+
+        <div class="form-group">
+            {!! Form::label('password', 'Password:') !!}
+            {!! Form::password('password', ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('role_id', 'Role:') !!}
+            {!! Form::select('role_id', [''=>'Choose Options'] + $roles, null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('is_active', 'Status:') !!}
+            {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not Active'), null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
             {!!  Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
         </div>
 
     {!!  Form::close() !!}
+
+    @include('includes.form_error')
+
+
 
 
     @stop
