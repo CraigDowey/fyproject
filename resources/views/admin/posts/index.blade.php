c52abe29caa4366e2ac60df814b4433df06d2bd9
Craig Dowey <Craig Dowey>
2017-02-28 13:02:50 +0000

    able to create posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -10,9 +10,9 @@
         <thead>
             <tr>
                 <th>ID</th>
+                <th>Photo</th>
                 <th>User</th>
                 <th>Category</th>
-                <th>Photo</th>
                 <th>Title</th>
                 <th>Body</th>
                 <th>Created</th>
@@ -26,9 +26,9 @@
             @foreach($posts as $post)
                 <tr>
                     <td>{{$post->id}}</td>
+                    <td><img height="75" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" ></td>
                     <td>{{$post->user->name}}</td>
-                    <td>{{$post->category_id}}</td>
-                    <td>{{$post->photo_id}}></td>
+                    <td>{{$post->category ? $post->category->name : 'Uncategorised'}}</td>
                     <td>{{$post->title}}</td>
                     <td>{{$post->body}}</td>
                     <td>{{$post->created_at->diffForHumans()}}</td>
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -34,7 +34,7 @@
             <td><img height="50" src="{{$user->photo ? $user->photo->file : '/images/imagename.php/400x400'}}" ></td>
             <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a> </td>
             <td>{{$user->email}}</td>
-            <td>{{$user->role_id}}</td>
+            <td>{{$user->role? $user->role->name : 'No Role'}}</td>
             <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
             <td>{{$user->created_at->diffForHumans()}}</td>
             <td>{{$user->updated_at->diffForHumans()}}</td>
